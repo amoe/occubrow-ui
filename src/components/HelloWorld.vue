03886c8c0fd1bb3a82fc15557f124efbd435e2da
David Banks <amoebae@gmail.com>
2018-12-21 17:05:53 +0000

    importing some data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,103 @@
 <template>
   <div class="hello">
-    
+    <svg width="600" :height="600">
+      <graph-view :width="600"
+                  :height="600"
+                  :x-margin="162"
+                  :y-margin="432"
+                  :depth-offset="120"
+                  :text-offset="22"
+                  :breadth="360"
+                  :graph-data="fakeGraphData"></graph-view>
+    </svg>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import {GraphView} from 'occubrow-graph-view';
+
+const FAKE_API_DATA = {
+    "children": [
+        {
+            "children": [
+                {
+                    "id": "St.",
+                    "taxon": null
+                },
+                {
+                    "id": "waggon",
+                    "taxon": null
+                }
+            ],
+            "id": "the",
+            "taxon": null
+        },
+        {
+            "children": [
+                {
+                    "id": "shop",
+                    "taxon": null
+                }
+            ],
+            "id": "a",
+            "taxon": null
+        }
+    ],
+    "id": "keep",
+    "taxon": null
+};
+
+const STATIC_TAXONOMY_DATA = {
+    "_type": "Taxon",
+    "children": [
+        {
+            "_type": "Taxon",
+            "children": [
+                {
+                    "_type": "Taxon",
+                    "id": 63754,
+                    "name": "Deepstaria enigmatica"
+                },
+                {
+                    "_type": "Taxon",
+                    "id": 63759,
+                    "name": "Deepstaria reticulum"
+                }
+            ],
+            "id": 63752,
+            "name": "Deepstaria"
+        },
+        {
+            "_type": "Taxon",
+            "children": [
+                {
+                    "_type": "Taxon",
+                    "id": 63760,
+                    "name": "Aurelia labiata"
+                },
+                {
+                    "_type": "Taxon",
+                    "id": 63761,
+                    "name": "Aurelia aurita"
+                }
+            ],
+            "id": 63753,
+            "name": "Aurelia"
+        }
+    ],
+    "id": 63751,
+    "name": "Ulmaridae"
+};
 
 export default Vue.extend({
     name: 'HelloWorld',
-    components: {}
+    data() {
+        return {
+            fakeGraphData: FAKE_API_DATA
+        };
+    },
+    components: {GraphView}
 });
 </script>
 
--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { GraphViewModule } from 'occubrow-graph-view';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
+    state: {
 
-  },
-  mutations: {
+    },
+    mutations: {
 
-  },
-  actions: {
+    },
+    actions: {
 
-  },
+    },
+    modules: {
+        graphView: GraphViewModule
+    }
 });
